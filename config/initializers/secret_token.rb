bc3f0eda3d9ca1847e15319b25a54b57a8da2971
Artem Shoobovych <ars@qapint.com>
2013-08-06 16:51:41 +0300

    Initial new design commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shybovycha::Application.config.secret_key_base = 'b1fdcb5e399a27e0446b6bd21246a81e261409e6cf7b0cde40fdada4ac57d18edec9bdd11230099d0802fbea871639fc6f9a25240efd16411efa92835563decb'
